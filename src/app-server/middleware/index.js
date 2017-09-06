2876a648c9c1e85ef33f9b681d71fb0db7bfc0ed
Kyle Welch <kwelch0626@gmail.com>
2017-09-06 16:13:45 -0500

    added middleware index for quick resolves

---
+++ src/app-server/middleware/index.js
@@ -0,0 +1,9 @@
+import appendLocalsMiddleware from './appendLocalsMiddleware';
+import proxySetupMiddleware from './proxySetupMiddleware';
+import tokenIssueMiddleware from './tokenIssueMiddleware';
+
+export default {
+  appendLocalsMiddleware,
+  tokenIssueMiddleware,
+  proxySetupMiddleware,
+};
